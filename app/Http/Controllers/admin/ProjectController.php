a6e5e4aebb257e3c844c08ab5877535688b5db46
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-10-11 01:12:12 +0200

    [UPD] Created  relation between two tables when user add new project

--- app/Http/Controllers/admin/ProjectController.php
+++ app/Http/Controllers/admin/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
@@ -30,7 +31,9 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        return view('admin.projects.create');
+        $types = Type::all();
+
+        return view('admin.projects.create',compact('types'));
     }
 
     /**
--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -26,7 +26,8 @@ class StoreProjectRequest extends FormRequest
         return [
             'name' => 'required|max:255',
             'image' => 'nullable|image|max:4084',
-            'summary' => 'nullable'
+            'summary' => 'nullable',
+            'type_id' => 'nullable'
         ];
     }
     public function message()
